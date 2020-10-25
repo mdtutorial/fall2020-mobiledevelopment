628814b0942bdf9be15b0e4b6028d0841b8f3811
omontel1 <70669023+omontel1@users.noreply.github.com>
2020-10-24 20:15:14 -0400

    Portfolio 1
    Week 6 Project

--- week6/src/Components/Example/index.js
+++ week6/src/Components/Example/index.js
@@ -1,9 +1,18 @@
 import React from 'react';
 
-const Example  = () => {
+function Example() {
     return (
-        <p>Hi, my name is Omar Montelongo</p>
-    )
-};
+        <div>
+        <h1>Welcome to Mobile Development!</h1>
+        <p><h2>Description</h2>Mobile application development is a booming field, with native applications for iOS and Android taking advantage of an ecosystem of features available on pocket-sized devices. In this course, students will apply their existing web development skills in HTML, CSS, and JavaScript to building native applications using React Native, a Facebook-developed framework.</p>
+        
+        <p><h2>Development Tool Installation</h2>The required resources for this course must be installed on a computer in order to be used correctly. If installing programs is not possible due to any reason, alternatives such as Repl.it, Snack, and other hosting services are encouraged for development purposes. Please let the instructor know if these services will be used, as certain code submission methods may not always be possible when using them.</p>
+        
+        <div class="footer">
+        <h4>Omar Montelongo</h4>
+        </div>  
+        </div>
+    );
+}
 
 export default Example;
